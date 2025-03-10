57d9fbf526abc74cebb718f665abbc18ef7cc082
Robert Hill <robert.hill@uphillsolutions.tech>
2025-03-10 13:17:12 -0400

    Add options for scrolling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.sidescrolloff = 8 -- Columns of context
