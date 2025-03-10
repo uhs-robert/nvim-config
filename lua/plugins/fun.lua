002ab6b2d9158d852cd28ac80c0ee07c4effcb65
Robert Hill <robert.hill@uphillsolutions.tech>
2025-03-10 13:18:41 -0400

    Disable player-one

--- lua/plugins/fun.lua
+++ lua/plugins/fun.lua
@@ -5,4 +5,19 @@ return {
     opts = {},
     cmd = { "Typr", "TyprStats" },
   },
+  -- {
+  --   "jackplus-xyz/player-one.nvim",
+  --   opts = {
+  --     ---@type boolean Whether the sound theme is enabled
+  --     is_enabled = false,
+  --
+  --     ---@type number Minimum interval between sounds in seconds
+  --     ---Prevents sound overlapping and potential audio flooding when
+  --     ---multiple keystrokes happen in rapid succession
+  --     min_interval = 0.05,
+  --
+  --     ---Available presets: "chiptune", "crystal", "synth"
+  --     theme = "chiptune",
+  --   },
+  -- },
 }
