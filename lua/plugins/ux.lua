8e30b34a0977757b7227242b46481ae94c2e3f78
Robert Hill <robert.hill@uphillsolutions.tech>
2025-06-14 22:16:07 -0400

    Add yazi with custom hook to change directory on selection of file

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -39,4 +39,57 @@ return {
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     lazy = false,
   },
+
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    -- dependencies = { "folke/snacks.nvim", lazy = true },
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>yy",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>yh",
+        -- mode = { "n", "v" },
+        function()
+          require("yazi").yazi(nil, "~")
+        end,
+        desc = "Open Yazi from home dir and return to previous cwd",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>yw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<leader>yt",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = true,
+      enable_mouse_support = true,
+      keymaps = {
+        show_help = "<f1>",
+      },
+      hooks = {
+        yazi_closed_successfully = function(chosen_file, config, state)
+          vim.notify(vim.inspect({ chosen_file, state.last_directory.filename }))
+        end,
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      -- vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
 }
