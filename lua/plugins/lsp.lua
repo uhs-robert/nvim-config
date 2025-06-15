4435a59333184ce7456931ac03fe50c5be0f6839
Robert Hill <robert.hill@uphillsolutions.tech>
2025-06-14 22:16:25 -0400

    Add LSP for javascript

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,40 @@
 return {
-  {
+  { -- plugin config for goto-preview
     "rmagatti/goto-preview",
     event = "LspAttach",
     config = function()
       require("goto-preview").setup({})
     end,
+  },
+
+  { -- extend LazyVim's LSP setup
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          init_options = {
+            hostInfo = "neovim",
+          },
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+          },
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
