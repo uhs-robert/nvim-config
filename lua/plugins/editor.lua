21f0dc8211c7aad5db2682072e446cf3a017951e
Robert Hill <robert.hill@uphillsolutions.tech>
2025-07-08 17:59:37 -0400

    Add nvim ufo
    Enables fold ugprades and modifications

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,17 +1,47 @@
 return {
 
-  -- {
-  --   "OXY2DEV/markview.nvim",
-  --   ft = "markdown",
-  --
-  --   dependencies = {
-  --     -- You may not need this if you don't lazy load
-  --     -- Or if the parsers are in your $RUNTIMEPATH
-  --     "nvim-treesitter/nvim-treesitter",
-  --
-  --     "nvim-tree/nvim-web-devicons",
-  --   },
-  -- },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    config = function()
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      require("ufo").setup({
+        provider_selector = function(_, _, _)
+          return { "treesitter", "indent" }
+        end,
+      })
+
+      local ufo = require("ufo")
+
+      -- Keymaps using WhichKey labels
+      vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+      vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds, { desc = "Open folds (skip comments/imports)" })
+      vim.keymap.set("n", "zm", function()
+        ufo.closeFoldsWith(1)
+      end, { desc = "Close folds (level 1)" })
+
+      vim.keymap.set("n", "K", function()
+        local winid = ufo.peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = "Peek folded lines or hover" })
+    end,
+  },
+
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>U", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
 
   {
     "shortcuts/no-neck-pain.nvim",
