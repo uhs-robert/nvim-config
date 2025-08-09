572b7ddf28fe9ea32bac07c3f59f605e26e23022
Robert Hill <robert.hill@uphillsolutions.tech>
2025-08-09 11:37:48 -0400

    chore: clean up plugin structure
    - Remove empty disabled.lua file
    - Rename neorg.lua to note-taking.lua for clarity

--- lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
--- Disabled plugins repository
--- Contains plugins that are temporarily or permanently disabled for reference
-return {
-  {},
-  -- { "echasnovski/mini.pairs", enabled = false },
-}
