6eca148b8d534a35bc2dfa43d97c512ecc5b5343
Robert Hill <robert.hill@uphillsolutions.tech>
2025-11-05 12:16:19 -0500

    fix: map leader key explicitly

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- init.lua
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.g.mapleader = " "
 require("config.lazy")
 
 vim.opt.path:append("**")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = " "
 vim.opt.wrap = true
 vim.opt.scrolloff = 8 -- Lines of context
 vim.opt.sidescrolloff = 8 -- Columns of context
