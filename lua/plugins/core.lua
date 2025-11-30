1899d7b67195ecaea0d3953b565f5daad0f922f2
Robert Hill <robert.hill@uphillsolutions.tech>
2025-11-29 20:16:51 -0500

    feat: add sudo colorscheme for root only

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,9 +1,10 @@
 -- lua/plugins/core.lua
+local is_root = vim.loop and vim.loop.getuid and vim.loop.getuid() == 0
 return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oasis",
+      colorscheme = is_root and "oasis-sol" or "oasis",
       news = {
         lazyvim = true,
         neovim = true,
