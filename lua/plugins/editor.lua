cfca5813cce3ec0fba36dc44593256f99a697938
Robert Hill <robert.hill@uphillsolutions.tech>
2026-01-29 20:09:48 -0500

    feat: expand treesitter config with explicit features and parsers
    Enable indent, highlight, and folds features explicitly. Add missing
    parsers: c, diff, jsdoc, jsonc, luadoc, luap, printf, toml, vimdoc, xml.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -8,21 +8,34 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      indent = { enable = true }, ---@type lazyvim.TSFeat
+      highlight = { enable = true }, ---@type lazyvim.TSFeat
+      folds = { enable = true }, ---@type lazyvim.TSFeat
       ensure_installed = {
-        "gotmpl",
         "bash",
+        "c",
+        "diff",
+        "gotmpl",
         "html",
         "javascript",
+        "jsdoc",
         "json",
+        "jsonc",
         "lua",
+        "luadoc",
+        "luap",
         "markdown",
         "markdown_inline",
+        "printf",
         "python",
         "query",
         "regex",
+        "toml",
         "tsx",
         "typescript",
         "vim",
+        "vimdoc",
+        "xml",
         "yaml",
       },
     },
