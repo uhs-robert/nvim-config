0acff1911fc5240d09f42fffbacca7a150ad3c19
Robert Hill <robert.hill@uphillsolutions.tech>
2026-02-02 10:42:01 -0500

    feat: add dd mapping to remove quickfix entries
    Add a quickfix FileType autocmd that binds dd to delete the
    
    current quickfix item and keep the cursor on a valid line.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,6 +31,33 @@ api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Quick Fix `dd` to delete items
+api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  desc = "Attach keymaps for quickfix list",
+  callback = function()
+    vim.keymap.set("n", "dd", function()
+      local qf_list = vim.fn.getqflist()
+
+      local current_line_number = vim.fn.line(".")
+
+      if qf_list[current_line_number] then
+        table.remove(qf_list, current_line_number)
+
+        vim.fn.setqflist(qf_list, "r")
+
+        local new_line_number = math.min(current_line_number, #qf_list)
+        vim.fn.cursor(new_line_number, 1)
+      end
+    end, {
+      buffer = true,
+      noremap = true,
+      silent = true,
+      desc = "Remove quickfix item under cursor",
+    })
+  end,
+})
+
 -- Hyprlang LSP
 api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.hl", "hypr*.conf" },
