725a3bed85040626afb7ed1ca4affd80cfaabfc6
Robert Hill <robert.hill@uphillsolutions.tech>
2026-02-05 17:23:25 -0500

    chore: add commented nvim-ts-autotag plugin reference
    Adds commented configuration for nvim-ts-autotag plugin (HTML/TS
    auto tag closing and renaming) for potential future use.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -122,4 +122,23 @@ return {
     "OXY2DEV/helpview.nvim",
     lazy = false,
   },
+
+  -- -- Html/Ts Autotag: Use treesitter to autoclose and autorename html tag
+  -- {
+  --   "windwp/nvim-ts-autotag",
+  --   opts = {
+  --     -- Defaults
+  --     enable_close = true, -- Auto close tags
+  --     enable_rename = true, -- Auto rename pairs of tags
+  --     enable_close_on_slash = false, -- Auto close on trailing </
+  --   },
+  --   -- Also override individual filetype configs, these take priority.
+  --   -- Empty by default, useful if one of the "opts" global settings
+  --   -- doesn't work well in a specific filetype
+  --   per_filetype = {
+  --     ["html"] = {
+  --       enable_close = false,
+  --     },
+  --   },
+  -- },
 }
